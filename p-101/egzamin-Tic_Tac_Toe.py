7b943761c0a1d1fff6a136dd937a821fbfb84e80
Michal-Z2J <wujas.gry@gmail.comgit config --global user.email wujas.gry@gmail.com>
2023-11-11 22:22:03 +0100

    Small fixes

--- p-101/egzamin-Tic_Tac_Toe.py
+++ p-101/egzamin-Tic_Tac_Toe.py
@@ -1,12 +1,12 @@
 # Egzamin p-101
 # Tic Tac Toe
-
 import random
 winning_combinations = ['123', '456', '789', '147', '258', '369', '159', '357']
 board_numbers = [num for num in range(1,10)]
 numbers = [str(numbers) for numbers in range(1,10)]
 player_x_numbers = []
 player_o_numbers = []
+
 def print_board():
     print(f'''
     {board_numbers[0]} | {board_numbers[1]} | {board_numbers[2]}
@@ -15,6 +15,7 @@ def print_board():
     - - - - -
     {board_numbers[6]} | {board_numbers[7]} | {board_numbers[8]}
     ''')    
+
 def check_winner(player, player_numbers):
     for combination in winning_combinations:
         if set([num for num in combination]).issubset(set(player_numbers)):
@@ -23,6 +24,7 @@ def check_winner(player, player_numbers):
     if set(board_numbers) == set(['X', 'O']):
         print("It's a draw")
         return True
+    
 def game_options(prompt, option1, option2):
     while True:
         user_input = input(prompt).upper()
@@ -31,6 +33,7 @@ def game_options(prompt, option1, option2):
         else:
             print("Wrong input, try again.")
     return user_input
+
 def player_field_numbers(player, move):
     if player == 'X':
         player_x_numbers.append(str(move))
@@ -38,6 +41,7 @@ def player_field_numbers(player, move):
     else:
         player_o_numbers.append(str(move))
         return player_o_numbers
+    
 def make_move(player):
     while True:
         move = input(f"Player {player} choose board number (1-9): ")
@@ -51,6 +55,7 @@ def make_move(player):
     print_board()
     if check_winner(player, player_numbers) == True:
         return True
+    
 def computer_move(computer):
     move = str(random.choice(board_numbers))
     while move in ['X', 'O']:
@@ -61,6 +66,7 @@ def computer_move(computer):
     print_board()
     if check_winner(computer, player_numbers) == True:
         return True
+    
 print("Welcome to Tic Tac Toe game.")
 game_type = game_options("Which game would you like to play? Enter '1' for player vs player or '2' for player vs computer: ", '1', '2')
 player_one = game_options("Player please choose 'X' or 'O' to play: ", 'X', 'O')
@@ -69,6 +75,7 @@ if player_one == 'X':
 else:
     player_two = 'X'
 print_board()
+
 while True:
     if make_move(player_one) == True:
         break
